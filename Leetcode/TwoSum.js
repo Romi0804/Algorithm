e3dd74af98f909dab550b50d0c03c97cd3d4cd0c
Ham Choromi <choromi0804@gmail.com>
2019-08-03 18:20:18 +0900

    initial commit

---
+++ Leetcode/TwoSum.js
@@ -0,0 +1,22 @@
+// Solution 1 - 49.95%
+
+const twoSum = function(nums, targer) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (targer === nums[i] + nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+// Soution 2  - 97.92% (hashtable)
+const twoSum = function(nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[target - nums[i]] != null) {
+      return [map[target - nums[i]], i];
+    }
+    map[nums[i]] = i;
+  }
+};
